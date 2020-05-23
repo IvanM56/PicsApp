6fb1d1cb584776f6bd79865ab9d1dbee437e5500
IvanM56 <ivan.miocic@gmail.com>
2020-05-23 19:20:53 +0200

    config ignored

--- app/Config.php
+++ app/Config.php
@@ -6,21 +6,21 @@ namespace App;
 class Config {
 
     /* DATABASE */
-    const DB_HOST = '';
-    const DB_NAME = '';
-    const DB_USER = '';
+    const DB_HOST = 'localhost';
+    const DB_NAME = 'picsapp';
+    const DB_USER = 'root';
     const DB_PASS = '';
 
 
     /* STORAGE */
-    const STORAGE = '';
+    const STORAGE = 'http://localhost:8080/picsapp_mvc/public/img/';
 
 
     /* URLROOT */
-    const ROOTURL = '';
+    const ROOTURL = 'http://localhost:8080/picsapp_mvc/';
 
     /* MAIL */
-    const EMAIL_ADDRESS = '';
-    const EMAIL_PASSWORD = '';
+    const EMAIL_ADDRESS = 'ivan.miocic2@gmail.com';
+    const EMAIL_PASSWORD = 'gecko2okceg';
     
 }
